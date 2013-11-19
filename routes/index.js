fff1937d5cb991cfc8e23936698dabda3100c19c
Alex Mark <ajmark@andrew.cmu.edu>
2013-11-18 21:59:39 -0500

    init

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var mongo = require("../models/mymongo.js");
+
+exports.get = function(req, res){
+		mongo.find( "nba", 
+		            "teams", 
+		            req.query,
+		            function(model) {
+		              res.render('index', {title: 'teams', obj: model});
+		              }
+		            );
+}
+
+exports.put = function(req, res){
+		mongo.insert( "nba", 
+		              "teams", 
+		              req.body,
+		              function(model) {
+		                res.render('index', {title: 'teams', obj: model});
+		                }
+		              );
+}
