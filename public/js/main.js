0ed4c0842bce667978fc8c307472d077a609c996
Alex Mark <ajmark@andrew.cmu.edu>
2013-11-29 22:30:32 -0500

    team news

--- public/js/main.js
+++ public/js/main.js
@@ -17,8 +17,9 @@ $(document).ready(function(){
 });
 
 function getLeagueNews() {
+	//gets json
 	$.ajax({
-		url: "http://api.espn.com/v1/sports/basketball/nba/news/?insider=no&limit=20&apikey=7efmy99vrj5rz4g35bmdep5u",
+		url: "http://api.espn.com/v1/sports/basketball/nba/news/?insider=no&limit=30&apikey=7efmy99vrj5rz4g35bmdep5u",
 		statusCode: {
 			400: function(){
 				$(".main-content").prepend("<h1>Could not load content. 400 error.</h1>")
@@ -28,7 +29,9 @@ function getLeagueNews() {
 			}	
 		}
 	}).done(function(data){
+		//clears the page
 		$("#headlines").empty();
+		//iterates through data and displays html
 		for(i=0;i<data.headlines.length;i++){
 			var id = data.headlines[i].id;
 			var headline = data.headlines[i].headline;
@@ -45,4 +48,45 @@ function getLeagueNews() {
 		};
 
 	}); 
+};
+
+function getTeamNews(teamid) {
+	//gets json
+	$.ajax({
+		url:"http://api.espn.com/v1/sports/basketball/nba/news/?limit=70&apikey=7efmy99vrj5rz4g35bmdep5u",
+		statusCode: {
+			400: function(){
+				$(".main-content").prepend("<h1>Could not load content. 400 error.</h1>")
+			},
+			404: function(){
+				$(".main-content").prepend("<h1>Could not load content. 404 error.</h1>")
+			}	
+		}
+	}).done(function(data){
+		console.log(document.activeElement.id);
+		console.log(data.headlines.length);
+		//clears the page
+		$("#headlines").empty();
+		//iterates through data and displays html
+		for(i=0;i<data.headlines.length;i++){
+			for(j=0;j<data.headlines[i].categories.length;j++){
+
+				if (data.headlines[i].categories[j].teamId === teamid){
+					var id = data.headlines[i].id;
+					var headline = data.headlines[i].headline;
+					var link = data.headlines[0].links.web.href;
+					var description = data.headlines[i].description;
+					$("#headlines").append("<div id='"+id+"' class='headline-box'></div>")
+					$("#"+id).append("<h2>"+headline+"</h2>");
+					if (data.headlines[i].images.length > 0){
+						var url = data.headlines[i].images[0].url;
+						$("#"+id).append("<img src='"+url+"'>");
+					};
+					$("#"+id).append("<p>"+description+"</p>");
+					$("#"+id).append("<a href='"+link+"' target='_blank'>Read More</a>")
+				};
+			};	
+		};
+
+	});
 };
