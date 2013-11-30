46f0677448a62341066d3349e245314d39ca9b3e
Alex Mark <ajmark@andrew.cmu.edu>
2013-11-30 01:07:14 -0500

    selector

--- public/js/main.js
+++ public/js/main.js
@@ -13,11 +13,13 @@ $(document).ready(function(){
 		$('.side-nav, .main-content').css('height', currHeight);
 
 	});
+	hideElements();
 	getLeagueNews();
+	selectorAction();
 });
 
 function getLeagueNews() {
-	//gets json
+	//gets json of headlines
 	$.ajax({
 		url: "http://api.espn.com/v1/sports/basketball/nba/news/?insider=no&limit=30&apikey=7efmy99vrj5rz4g35bmdep5u",
 		statusCode: {
@@ -44,16 +46,16 @@ function getLeagueNews() {
 				$("#"+id).append("<img src='"+url+"'>");
 			};
 			$("#"+id).append("<p>"+description+"</p>");
-			$("#"+id).append("<a href='"+link+"' target='_blank'>Read More</a>")
+			$("#"+id).append("<a href='"+link+"' target='_blank'>Continue reading...</a>")
 		};
 
 	}); 
 };
 
 function getTeamNews(teamid) {
-	//gets json
+	//gets json of headlines
 	$.ajax({
-		url:"http://api.espn.com/v1/sports/basketball/nba/news/?limit=70&apikey=7efmy99vrj5rz4g35bmdep5u",
+		url:"http://api.espn.com/v1/sports/basketball/nba/news/?limit=50&apikey=7efmy99vrj5rz4g35bmdep5u",
 		statusCode: {
 			400: function(){
 				$(".main-content").prepend("<h1>Could not load content. 400 error.</h1>")
@@ -63,30 +65,81 @@ function getTeamNews(teamid) {
 			}	
 		}
 	}).done(function(data){
-		console.log(document.activeElement.id);
-		console.log(data.headlines.length);
+		headlinedata = data;
+		document.getElementById("title").innerHTML = document.activeElement.id + " News";
+
 		//clears the page
 		$("#headlines").empty();
-		//iterates through data and displays html
-		for(i=0;i<data.headlines.length;i++){
-			for(j=0;j<data.headlines[i].categories.length;j++){
-
-				if (data.headlines[i].categories[j].teamId === teamid){
-					var id = data.headlines[i].id;
-					var headline = data.headlines[i].headline;
-					var link = data.headlines[0].links.web.href;
-					var description = data.headlines[i].description;
+		//iterates through headline data and displays html
+		for(i=0;i<headlinedata.headlines.length;i++){
+			for(j=0;j<headlinedata.headlines[i].categories.length;j++){
+				if (headlinedata.headlines[i].categories[j].teamId === teamid){
+					var id = headlinedata.headlines[i].id;
+					var headline = headlinedata.headlines[i].headline;
+					var link = headlinedata.headlines[0].links.web.href;
+					var description = headlinedata.headlines[i].description;
 					$("#headlines").append("<div id='"+id+"' class='headline-box'></div>")
 					$("#"+id).append("<h2>"+headline+"</h2>");
-					if (data.headlines[i].images.length > 0){
-						var url = data.headlines[i].images[0].url;
+					if (headlinedata.headlines[i].images.length > 0){
+						var url = headlinedata.headlines[i].images[0].url;
 						$("#"+id).append("<img src='"+url+"'>");
 					};
 					$("#"+id).append("<p>"+description+"</p>");
-					$("#"+id).append("<a href='"+link+"' target='_blank'>Read More</a>")
+					$("#"+id).append("<a href='"+link+"' target='_blank'>Continue reading...</a>")
 				};
 			};	
-		};
-
+		}; 
 	});
 };
+
+function hideElements(){
+	$(".side-nav").hide();
+	$(".groupings").hide();
+}
+
+function selectorAction() {
+	//When cursor enters and exits selector tab
+	$("#selector").mouseenter(function(){
+		$(".side-nav").show();
+	});
+	$(".side-nav").mouseleave(function(){
+		$(".side-nav").hide();
+	});
+
+	//sidebar navigation
+	$("#atlantic").click(function(){
+		$("#atlantic-div").show();
+		$("#divisions").hide();
+	});
+	$("#central").click(function(){
+		$("#central-div").show();
+		$("#divisions").hide();
+	});
+	$("#southeast").click(function(){
+		$("#southeast-div").show();
+		$("#divisions").hide();
+	});
+	$("#northwest").click(function(){
+		$("#northwest-div").show();
+		$("#divisions").hide();
+	});
+	$("#pacific").click(function(){
+		$("#pacific-div").show();
+		$("#divisions").hide();
+	});
+	$("#southwest").click(function(){
+		$("#southwest-div").show();
+		$("#divisions").hide();
+	});
+
+	//go back
+	$(".back").click(function(){
+		$(".groupings").hide();
+		$("#divisions").show();
+	});
+}
+
+
+
+
+
